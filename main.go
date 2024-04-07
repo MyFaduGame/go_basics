46e47dd80e448f86eceb9b122abf445681290dc2
Navin319931 <navinsharma9376319931@gmail.com>
2024-04-07 13:28:13 +0530

    Added arrays and slices

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 	// for_loop()
 	// if_else()
 	// switch_case()
-	arrays_example()
+	// arrays_example()
+	// slices_example()
 }
 
 func constants() {
@@ -134,4 +135,81 @@ func switch_case() {
 func arrays_example() {
 	var a [5]int
 	fmt.Println(a)
+
+	//seting value to the array
+	a[4] = 100
+	fmt.Println("set",a)
+
+	//to print specific element
+	fmt.Println("get",a[4])
+
+	//smillar as rest of arrays
+	var twoD [2][3]int
+    for i := 0; i < 2; i++ {
+        for j := 0; j < 3; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
+}
+
+
+func slices_example() {
+
+    var s []string
+    fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	/*
+	To create an empty slice with non-zero length, use the builtin make. 
+	Here we make a slice of strings of length 3 (initially zero-valued). 
+	By default a new slice’s capacity is equal to its length; 
+	if we know the slice is going to grow ahead of time, 
+	it’s possible to pass a capacity explicitly as an additional parameter to make.
+	*/
+
+    s = make([]string, 3)
+    fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+    s[0] = "a"
+    s[1] = "b"
+    s[2] = "c"
+    fmt.Println("set:", s)
+    fmt.Println("get:", s[2])
+
+    fmt.Println("len:", len(s))
+
+    s = append(s, "d")
+    s = append(s, "e", "f")
+    fmt.Println("apd:", s)
+
+    c := make([]string, len(s))
+    copy(c, s)
+    fmt.Println("cpy:", c)
+
+    l := s[2:5]
+    fmt.Println("sl1:", l)
+
+    l = s[:5]
+    fmt.Println("sl2:", l)
+
+    l = s[2:]
+    fmt.Println("sl3:", l)
+
+    t := []string{"g", "h", "i"}
+    fmt.Println("dcl:", t)
+
+    t2 := []string{"g", "h", "i"}
+    if slices.Equal(t, t2) {
+        fmt.Println("t == t2")
+    }
+
+    twoD := make([][]int, 3)
+    for i := 0; i < 3; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
 }
